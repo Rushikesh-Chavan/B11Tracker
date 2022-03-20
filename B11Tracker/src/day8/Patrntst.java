658f75debe3f4d84759cda415a28796a26d85231
Rushikesh <Rushikesh@192.168.0.103>
2022-03-20 15:06:26 +0530

    commit4

---
+++ B11Tracker/src/day8/Patrntst.java
@@ -0,0 +1,20 @@
+package day8;
+
+public class Patrntst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int lines=10;  
+		int i=1;  
+		int j;  
+		for(i=1;i<=lines;i++){// this loop is used to print the lines  
+		for(j=1;j<=i;j++){// this loop is used to print lines  
+		System.out.print(i*j+" ");  
+		}  
+		System.out.println("");  
+		}  
+
+	}
+
+}
